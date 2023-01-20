254aa4174e39894505302fe7f693e7aeef053e45
zienabalawneh <yahyazainab204@gmail.com>
2023-01-20 16:10:46 +0200

    .

---
+++ javascript/app.js
@@ -0,0 +1,135 @@
+// var FirstNam = prompt("Please enter your name");
+var x = 5;
+var y = 84;
+var max = 0;
+if (x >= y)
+    document.write("max is x or equal ");
+else
+    document.write("max is " + y);
+
+
+
+
+// --------------------------------------SONG&AGE-----------------------------------------------------
+// var userName = prompt("enter your name ");
+// Name();
+
+
+// var Age;
+// var FavSonge;
+// getInfo(Age, FavSonge);
+
+// function Name() {
+
+//     var sayHi;
+
+//     if (userName == "" || userName == null)
+//         sayHi = "hi eveyone";
+//     else
+//         sayHi = "hi " + userName
+
+//     console.log("Hello world!" + sayHi);
+//     alert(sayHi);
+//     // if(userName==""||userName==null)
+//     // alert("hi ");
+//     // else 
+//     // alert("hi "+userName);
+
+// }
+
+
+
+
+// function getInfo(age, Songe) {
+
+
+//     age = prompt("enter your age  ")
+//     while (age == 0 || age == null || age < 0 || age === "") {
+//         age = prompt("enter your age pl... ");
+//     }
+
+
+//     //  while(age==0 || age==null||age<0){
+
+//     // age= prompt("Please tell me how old are you?");
+//     // }
+
+
+
+//     Songe = prompt("enter your Fav song (sad/happy/comedies/adventures) ");
+
+//     while (Songe !== "sad" && Songe !== "happy" && Songe !== "comedies" && Songe !== "adventures") {
+//         Songe = prompt("enter your Fav song ");
+//     }
+
+
+
+//     if (age < 18) {
+
+
+//         while (Songe !== "happy" && Songe !== "comedies") {
+//             // document.write("bad choese , happy comedies");
+//             Songe = prompt("enter your Fav song  bad choese , happy comedies ");
+
+
+//         }
+
+//         if (Songe == "happy" || Songe == "comedies") {
+
+//             document.write("good choese");
+
+//         }
+
+
+
+
+//     }
+
+//     if (age >= 18) {
+
+
+
+//         if (Songe === "happy" || Songe === "comedies") {
+
+//             document.write('<a href="https://www.youtube.com/watch?v=GICakUjhHa4">' + "happy comdy" + '</a>');
+
+//         } else if (Songe === "sad") {
+
+//             console.log("Hello world!");
+
+//             document.write('<a href="https://www.youtube.com/watch?v=laaargEoAz0">' + "sad" + '</a>');
+
+//         } else if (Songe === "adventures") {
+
+//             document.write("good choese adventures");
+//         }
+
+
+
+//     }
+
+
+
+
+// }
+
+
+
+//------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------
+
+//  document.write(); عشان اكتب عالصفحة نفسها 
+// prompt() ; عشان ادخل اشي في popwindow
+//alert يطبع في popwindow
+// console.log(); جوا ال inspact
+// ==بدون ما ارعي القيمة 
+//=== مع مراعة القيمة 
